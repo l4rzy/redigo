a44168876302654c7d3dbdd59e88b2b52e743291
l4rzy <l4.foss@gmail.com>
2019-12-17 17:29:39 +0700

    add other interfaces to assemblage

--- redis/assemblage.go
+++ redis/assemblage.go
@@ -5,8 +5,7 @@ import (
 )
 
 // an assemblage is a collection of pools, each of these pool connects to a redis
-// node that has an exact copy of a "master" node, just like RAID-1 but for redis
-
+// node that has an exact copy of a "master" node, just like RAID-1 but for redi
 type Assemblage struct {
 	num_pools int // number of pools in an assemblage
 	pools     []*Pool
@@ -32,4 +31,34 @@ func (a *Assemblage) Get() Conn {
 	a.i = (a.i + 1) % a.num_pools
 	a.mu.Unlock()
 	return a.pools[a.i].Get()
+}
+
+func (a *Assemblage) Close() {
+	a.mu.Lock()
+	for i := 0; i < a.num_pools; i++ {
+		a.pools[i].Close()
+	}
+	a.mu.Unlock()
+}
+
+func (a *Assemblage) ActiveCount() int {
+	var count int = 0
+	for i := 0; i < a.num_pools; i++ {
+		a.mu.Lock()
+		count += a.pools[i].ActiveCount()
+		a.mu.Unlock()
+	}
+
+	return count
+}
+
+func (a *Assemblage) IdleCount() int {
+	var count int = 0
+	for i := 0; i < a.num_pools; i++ {
+		a.mu.Lock()
+		count += a.pools[i].IdleCount()
+		a.mu.Unlock()
+	}
+
+	return count
 }
