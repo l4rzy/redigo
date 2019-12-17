cc5c3f8412cf50dbe870b1e907057937cc883938
l4rzy <l4.foss@gmail.com>
2019-12-17 17:01:10 +0700

    remove adaptive picking, and use round robin in all cases

--- redis/assemblage.go
+++ redis/assemblage.go
@@ -7,11 +7,6 @@ import (
 // an assemblage is a collection of pools, each of these pool connects to a redis
 // node that has an exact copy of a "master" node, just like RAID-1 but for redis
 
-const (
-	ASSEMBLAGE_ROUND_ROBIN = 0
-	ASSEMBLAGE_ADAPTIVE    = 1
-)
-
 type Assemblage struct {
 	num_pools int // number of pools in an assemblage
 	pools     []*Pool
@@ -32,37 +27,9 @@ func (a *Assemblage) AddPool(p *Pool) {
 	a.num_pools++
 }
 
-// no need to consistently return a pool, just pick the one with the most idle connections
-func (a *Assemblage) Get(kind int) Conn {
-	switch kind {
-	case ASSEMBLAGE_ROUND_ROBIN:
-		return a.getRoundRobin()
-	case ASSEMBLAGE_ADAPTIVE:
-		return a.getAdaptive()
-	}
-	// default case
-	return a.getRoundRobin()
-}
-
-func (a *Assemblage) getRoundRobin() Conn {
-	// no need to call mutex here since num_pools is a field
-	// a.mu.Lock()
+func (a *Assemblage) Get() Conn {
+	a.mu.Lock()
 	a.i = (a.i + 1) % a.num_pools
-	// a.mu.Unlock()
+	a.mu.Unlock()
 	return a.pools[a.i].Get()
-}
-
-func (a *Assemblage) getAdaptive() Conn {
-	var max_idle_i = 0
-
-	// not really sure about efficiency of this over round robin, since this calls
-	// mutex lock and unlock exactly `num_pools` times, whereas RR is O(1)
-	// regardless `num_pools`
-	for i := 1; i < a.num_pools; i++ {
-		if a.pools[i].IdleCount() >= max_idle_i {
-			max_idle_i = i
-		}
-	}
-
-	return a.pools[max_idle_i].Get()
 }
